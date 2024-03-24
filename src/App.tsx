5f788f449088862e92a2dea2d997e5c66938262e
Роман Литвиненко <romantolstyaklitvinenko@gmail.com>
2024-03-25 00:39:43 +0600

    fixed active link

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,26 @@
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Comics from './routes/Comics';
 import ComicsDetails from "./routes/Comics/ComicsDetails.tsx";
 import Characters from './routes/Characters';
 import CharacterDetails from './routes/Characters/CharacterDetails.tsx';
 import Layout from './components/Layout';
 import './App.css';
-
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Layout />}>
+                <Route
+                    path="/"
+                    element={<Layout />}
+                    className={(match, location) => (location.pathname === '/' || location.pathname === '/characters') ? 'active' : ''}
+                >
                     <Route index element={<Characters />} /> {/*чтобы пока что на главной странице были characters*/}
                     <Route path="/comics" element={<Comics />} />
-                    <Route path="/comics/:id" element={<ComicsDetails />} />
+                    <Route path="/comics/:id" element={ <ComicsDetails />} />
                     <Route path="/characters" element={<Characters />} />
                     <Route path="/characters/:id" element={<CharacterDetails />} />
                 </Route>
             </Routes>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/characters" activeClassName="active">
-                            Characters
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/comics" activeClassName="active">
-                            Comics
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
         </Router>
     );
 }
