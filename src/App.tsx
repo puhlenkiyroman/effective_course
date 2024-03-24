59e875b785ebcbc61baf6688b0286248ce2c00a6
Роман Литвиненко <romantolstyaklitvinenko@gmail.com>
2024-03-25 00:34:17 +0600

    added active link

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 import Comics from './routes/Comics';
 import ComicsDetails from "./routes/Comics/ComicsDetails.tsx";
 import Characters from './routes/Characters';
 import CharacterDetails from './routes/Characters/CharacterDetails.tsx';
 import Layout from './components/Layout';
 import './App.css';
+
 function App() {
     return (
         <Router>
@@ -12,11 +13,25 @@ function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Characters />} /> {/*чтобы пока что на главной странице были characters*/}
                     <Route path="/comics" element={<Comics />} />
-                    <Route path="/comics/:id" element={ <ComicsDetails />} />
+                    <Route path="/comics/:id" element={<ComicsDetails />} />
                     <Route path="/characters" element={<Characters />} />
                     <Route path="/characters/:id" element={<CharacterDetails />} />
                 </Route>
             </Routes>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/characters" activeClassName="active">
+                            Characters
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/comics" activeClassName="active">
+                            Comics
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
         </Router>
     );
 }
