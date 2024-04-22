9111b0ef79d395b907f63b61603249052a57d96f
Роман Литвиненко <romantolstyaklitvinenko@gmail.com>
2024-04-22 17:26:57 +0600

    added automatic deletion of a card after removing a like without refreshing the page

--- src/routes/Favourites/Favourites.tsx
+++ src/routes/Favourites/Favourites.tsx
@@ -1,16 +1,20 @@
-import {useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Card from '../../components/Card';
 import styles from './Favourites.module.css';
+import { IComic } from '../../types/comics';
+import { ICharacter } from '../../types/characters';
+
+//Types
+type FavoriteItem = IComic | ICharacter;
 
 function Favourites() {
-    const [favoriteItems, setFavoriteItems] = useState<object[]>([]);
+    const [favoriteItems, setFavoriteItems] = useState<FavoriteItem[]>([]);
 
     useEffect(() => {
         const likedIds = Object.keys(localStorage)
             .filter(key => key.startsWith('liked_'))
             .map(key => key.replace('liked_', ''));
-        const items = likedIds.map((id: string) => {
+        const items: FavoriteItem[] = likedIds.map((id: string) => {
             const savedItemString = localStorage.getItem(`liked_${id}`);
             if (savedItemString) {
                 const item = JSON.parse(savedItemString);
@@ -18,22 +22,30 @@ function Favourites() {
                 return item;
             }
             return null;
-        });
+        }).filter((item): item is FavoriteItem => item !== null);
 
         setFavoriteItems(items);
-        console.log(items)
     }, []);
+
+    // Обработчик для удаления карточки из избранного
+    const handleRemoveFromFavorites = (id: number) => {
+        setFavoriteItems(prevItems => prevItems.filter(item => item.id !== id));
+    };
 
     return (
         <>
             <h1>Favourites <span className={styles.favouritesCount}>({favoriteItems.length})</span></h1>
-                <div className={styles.favourites_container}>
-                    {favoriteItems.map(item => (
-                        <Link key={item.id} to={`/${item.type}/${item.id}`} className={styles.favourite_link}>
-                            <Card card={item} />
-                        </Link>
-                    ))}
-                </div>
+            <div className={styles.favourites_container}>
+                {favoriteItems.map(item => (
+                    <div key={item.id} className={styles.favourite_link}>
+                        <Card
+                            card={item}
+                            onLike={() => handleRemoveFromFavorites(item.id)}
+                            onClick={() => {}}
+                        />
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
