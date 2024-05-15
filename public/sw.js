13e74b97cefd473c1a283c1a88300c824a5ebb6e
Роман Литвиненко <romantolstyaklitvinenko@gmail.com>
2024-05-15 17:59:07 +0600

    manifest.json added + sw.js updated

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,49 @@
+const CACHE_NAME = 'marvel-app';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/src/main.tsx',
+    '/index.js',
+];
+
+// Установка и кэширование ресурсов
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Обработка запросов на ресурсы
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Обновление кэша при активации нового service worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Обработка push-уведомлений
 const receivePushNotification = (event) => {
     const { title, body, data, actions } = event.data.json();
 
@@ -10,6 +56,7 @@ const receivePushNotification = (event) => {
     event.waitUntil(self.registration.showNotification(title, options));
 };
 
+// Обработка кликов по уведомлениям
 const handleClickNotification = (event) => {
     if (event.action === 'close') {
         event.notification.close();
