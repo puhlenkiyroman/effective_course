13da14c411e07853dbf8e2bb8cdb7e2a0d9e7861
Роман Литвиненко <romantolstyaklitvinenko@gmail.com>
2024-06-06 17:59:05 +0600

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Comics from './routes/Comics';
 import ComicsDetails from "./routes/Comics/ComicsDetails";
 import Characters from './routes/Characters';
