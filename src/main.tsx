7cfebd31fce8aa56c3323f9865c46169065906ac
Роман Литвиненко <romantolstyaklitvinenko@gmail.com>
2024-06-07 18:27:11 +0600

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 // Localization
 import i18n from './localization';
@@ -11,8 +11,8 @@ i18n.init();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <BrowserRouter basename={'/effective_course'}>
+        <HashRouter basename={'/effective_course'}>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
